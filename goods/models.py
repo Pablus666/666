2a74337390bed33f508bae5a9690ab9e8eebb3a8
02 <Bear063.malafeev@yandex.ru>
2024-05-22 00:05:51 +0400

    Далее: заполнение таблиц admin 4:14:00

--- goods/models.py
+++ goods/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
-    clug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
 
     class Meta:
         db_table = 'category'
