0d2b0682088ab3a4de9ae5265e270f5ffb32ac7e
02 <Bear063.malafeev@yandex.ru>
2024-05-26 01:00:21 +0400

    Фикстуры для goods | 4:38:00

--- goods/models.py
+++ goods/models.py
@@ -10,13 +10,16 @@ class Categories(models.Model):
         verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
 
+    def __str__(self) -> str:
+        return self.name
+
 class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
     description = models.TextField(blank=True, null= True, verbose_name='Описание')
     image = models.ImageField(upload_to='goods_images',blank=True, null= True, verbose_name='Изображение')
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
-    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в %')
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='Скидка в %')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
 
@@ -25,3 +28,6 @@ class Products(models.Model):
         db_table = 'Product'
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
+    def __str__(self) -> str:
+        return f'{self.name} Количество - {self.quantity}'
