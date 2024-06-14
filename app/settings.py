3e269046bde408fbb206e001e2bc6509e8664144
02 <Bear063.malafeev@yandex.ru>
2024-06-14 23:48:33 +0400

    Измененеие полнотекстового поиска 8:35:00

--- app/settings.py
+++ app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
+    "django.contrib.postgres",
+
     'debug_toolbar',
 
     'main',
