688808a1540592f3d8db87c8e0504057abe5e68a
02 <Bear063.malafeev@yandex.ru>
2024-07-08 21:51:42 +0400

    Регистрационная форма и контреллер готов. Не работает регистрация, возрвщает заполненную форму, а не на сайт. 11:04:00

--- users/views.py
+++ users/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from users.forms import UsersLoginForm
+from users.forms import UserRegistrationForm
+from django.shortcuts import redirect
 
 
 def login(request):
@@ -25,7 +27,17 @@ def login(request):
 
 
 def registration(request):
-    context = {"title": "Home - Регистрация"}
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserRegistrationForm()
+
+    context = {"title": "Home - Регистрация", "form": form}
     return render(request, "users/registration.html", context)
 
 
@@ -34,4 +46,6 @@ def profile(request):
     return render(request, "users/profile.html", context)
 
 
-def logout(request): ...
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('main:index'))
